f242b2d2bc4d444836f1f88c30edee54d46012e7
JoeriRoijenga <roijenga.joeri@gmail.com>
2021-09-18 11:25:24 +0200

    setup first week

---
+++ startcode bij weekopgaven(3)/week 1/rivercrossing.py
@@ -0,0 +1,38 @@
+import time
+
+left = ["F", "G", "C", "W"]
+right = []
+
+rules = {
+    "G":["C", "W"],
+    "W":["G"],
+    "C":["G"],
+}
+
+def printStates():
+    print('States:')
+    print('left: ', left)
+    print('right: ', right)
+    print()
+
+if __name__ == "__main__":    
+    run = True
+    while run:
+        printStates()
+        right.insert(0, left.pop(0))
+
+        for rule in rules[left[0]]:
+            if rule in right: 
+                left.append(right.pop(right.index(rule)))
+                break
+
+        right.append(left.pop(0))
+        
+        if len(left) > 0:
+            printStates()
+            left.insert(0, right.pop(0))
+        else:
+            run = False
+        time.sleep(1)
+    print('Done')
+    printStates()
