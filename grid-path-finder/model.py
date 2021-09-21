6b9321249dac1595df73b9bbdc312e6e8706d533
Joost Blaauwwiekel <keeperjoost@gmail.com>
2021-09-21 11:55:19 +0200

    Assignment 4 works

--- grid-path-finder/model.py
+++ grid-path-finder/model.py
@@ -42,6 +42,7 @@ def set_grid_value(node, value):
 
 
 def heuristic(start, goal):
+    """
     # https://stackoverflow.com/questions/46974075/a-star-algorithm-distance-heuristics
     D = 1
     D2 = math.sqrt(2)
@@ -50,7 +51,8 @@ def heuristic(start, goal):
     dy = goal[1] - start[1]
 
     return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
-
+    """
+    return abs(goal[0] - start[0]) + abs(goal[1] - start[1])
 
 def get_cost(node):
     cost = get_grid_value(node)
@@ -115,26 +117,27 @@ def search(app, start, goal):
         # If the goal is reached
         if current_node == goal:
             print(final_path)
-            draw_line(app, final_path)
+            app.draw_path(final_path)
             break
 
         visited.append(current_node)
 
         for neighbour in neighbours:
 
-            new_cost = heuristic(current_node, goal)
+            new_cost = get_cost(current_node) + get_cost(neighbour)
             print(neighbour, new_cost)
 
-            if neighbour not in visited or new_cost < heuristic(neighbour, goal):
+            if neighbour not in visited or new_cost < get_cost(neighbour):
 
                 priority = new_cost + heuristic(neighbour, goal)
                 priority_queue.put(neighbour, priority)
 
                 visited.append(neighbour)
-                final_path[current_node] = neighbour
+                final_path[neighbour] = current_node
 
 
 def draw_line(app, path):
     for first_node, second_node in path.items():
         app.plot_line_segment(first_node[0], first_node[1], second_node[0], second_node[1], color=cf.FINAL_C)
         app.pause()
+        sleep(0.5)
