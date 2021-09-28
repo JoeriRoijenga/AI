b473ad1bedec6b19c0b72d525753b1c9452639f0
Joost Blaawwiekel <keeperjoost@gmail.com>
2021-09-28 14:55:22 +0200

    Assignment 1 and 3 almost done

--- 2048/model.py
+++ 2048/model.py
@@ -4,6 +4,12 @@ import math
 
 MAX_DEPTH = 3
 
+weigths = [  0,  0,  1,  3,
+             0,  1,  3,  5,
+             1,  3,  5,  15,
+             3,  5,  15, 30]
+
+
 def merge_left(b):
     # merge the board left
     # this function is reused in the other merges
@@ -37,6 +43,7 @@ def merge_left(b):
     # return [[2, 8, 0, 0], [2, 4, 8, 0], [4, 0, 0, 0], [4, 4, 0, 0]]
     return new_b
 
+
 def merge_right(b):
     # merge the board right
     # b = [[0, 2, 4, 4], [0, 2, 4, 8], [0, 0, 0, 4], [2, 2, 2, 2]]
@@ -50,6 +57,7 @@ def merge_right(b):
     # return [[0, 0, 2, 8], [0, 2, 4, 8], [0, 0, 0, 4], [0, 0, 4, 4]]
     return [reverse(x) for x in ml]
 
+
 def merge_up(b):
     # merge the board upward
     # note that zip(*b) is the transpose of b
@@ -59,11 +67,13 @@ def merge_up(b):
     # return [[2, 4, 8, 4], [0, 2, 2, 8], [0, 0, 0, 4], [0, 0, 0, 2]]
     return [list(x) for x in zip(*trans)]
 
+
 def merge_down(b):
     # merge the board downward
     trans = merge_right(zip(*b))
     # return [[0, 0, 0, 4], [0, 0, 0, 8], [0, 2, 8, 4], [2, 4, 2, 2]]
     return [list(x) for x in zip(*trans)]
+
 
 # location: after functions
 MERGE_FUNCTIONS = {
@@ -72,6 +82,7 @@ MERGE_FUNCTIONS = {
     'up': merge_up,
     'down': merge_down
 }
+
 
 def move_exists(b):
     # check whether or not a move exists on the board
@@ -92,6 +103,7 @@ def move_exists(b):
     else:
         return False
 
+
 def start():
     # make initial board
     b = [[0] * 4 for _ in range(4)]
@@ -99,11 +111,13 @@ def start():
     add_two_four(b)
     return b
 
+
 def play_move(b, direction):
     # get merge functin an apply it to board
     b = MERGE_FUNCTIONS[direction](b)
     add_two_four(b)
     return b
+
 
 def add_two_four(b):
     # add a random tile to the board at open position.
@@ -118,13 +132,15 @@ def add_two_four(b):
             return (b)
         else:
             continue
-            
+
+
 def game_state(b):
     for i in range(4):
         for j in range(4):
             if b[i][j] >= 2048:
                 return 'win'
     return 'lose'
+
 
 def test():
     b = [[0, 2, 4, 4], [0, 2, 4, 8], [0, 0, 0, 4], [2, 2, 2, 2]]
@@ -146,8 +162,104 @@ def test():
         add_two_four(b)
         print(b)
 
-def get_random_move():
-    return random.choice(list(MERGE_FUNCTIONS.keys()))
 
-def get_expectimax_move(b):
-    pass
+def get_random_move():
+    return random.choice(list(MERGE_FUNCTIONS.keys())), -1
+
+
+def get_expectimax_move(b, depth, player):
+    if depth == 0:
+        return -1, heuristic_corner(b) + heuristic_monotonicity(b) + heuristic_empty_cells(b)
+
+    if player is True:
+        best_value = -100000
+        best_move = ""
+
+        for choice in MERGE_FUNCTIONS.keys():
+            temp_board = b.copy()
+            temp_board = play_move(temp_board, choice)
+
+            _, value = get_expectimax_move(temp_board, depth - 1, False)
+
+            if value > best_value:
+                best_value = value
+                best_move = choice
+
+        return best_move, best_value
+    else:
+        best_value = 100000
+        temp_b = b.copy()
+
+        _, value = get_expectimax_move(temp_b, depth - 1, True)
+        final_score = min(value, best_value)
+
+        return -1, final_score
+
+
+def heuristic_corner(b):
+    total_value = 0
+    index = 0
+
+    # Hold all the most valuable tiles in one corner
+    for row in b:
+        for item in row:
+            if item != 0:
+                total_value = total_value + (weigths[index] * 2)
+            index += 1
+
+    return total_value
+
+
+def heuristic_empty_cells(b):
+    # Try to keep as many empty cells as possible
+    total_value = 0
+    for x in range(4):
+        for y in range(4):
+            if b[x][y] == 0:
+                total_value += 8
+
+    return total_value
+
+
+def heuristic_penalty(b):
+    penalty = 0
+
+    # Try to cluster the highest together
+    for x in range(4):
+        for y in range(4):
+            for neighbour in get_neighbours(b, x, y):
+                penalty = penalty + (b[x][y] - neighbour)
+
+    return penalty
+
+
+def heuristic_monotonicity(b):
+    total_value = 0
+
+    for x in range(4):
+        for y in range(4):
+            for neighbour in get_neighbours(b, x, y):
+                if neighbour == b[x][y]:
+                    total_value += 15
+
+                if neighbour == b[x][y] / 2:
+                    total_value += 10
+
+                if neighbour == b[x][y] / 4:
+                    total_value += 5
+
+    return total_value
+
+
+def get_neighbours(matrix, rowNumber, colNumber):
+    result = []
+    for rowAdd in range(-1, 2):
+        newRow = rowNumber + rowAdd
+        if newRow >= 0 and newRow <= len(matrix)-1:
+            for colAdd in range(-1, 2):
+                newCol = colNumber + colAdd
+                if newCol >= 0 and newCol <= len(matrix)-1:
+                    if newCol == colNumber and newRow == rowNumber:
+                        continue
+                    result.append(matrix[newCol][newRow])
+    return result
